a54a1ec1950a11952de322f898772fe1a8457a3f
Tiago Ceccon <ceccon.t@hotmail.com>
2020-03-19 23:19:02 -0300

    Sunset old config class that used arbitrary properties files

---
+++ src/main/java/guru/springframework/config/ArbitraryPropertiesFilePropertyConfig.java
@@ -0,0 +1,70 @@
+package guru.springframework.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
+
+//@Configuration
+// Multiple sources alternative 1:
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+// Multiple sources alternative 2 (introduced on Spring 4):
+@PropertySources({
+		@PropertySource("classpath:datasource.properties"),
+		@PropertySource("classpath:jms.properties")
+})
+public class ArbitraryPropertiesFilePropertyConfig {
+	
+	@Autowired
+	Environment env;
+	
+	@Value("${guru.username}")
+	String user;
+	
+	@Value("${guru.pass}")
+	String pass;
+	
+	@Value("${guru.dburl}")
+	String url;
+	
+	@Value("${guru.jms.username}")
+	String jmsUsername;
+	
+	@Value("${guru.jms.pass}")
+	String jmsPass;
+	
+	@Value("${guru.jms.url}")
+	String jmsUrl;
+	
+	@Bean
+	public FakeDataSource fakeDataSource() {
+		FakeDataSource fakeDataSource = new FakeDataSource();
+		fakeDataSource.setUser(env.getProperty("GURU_USERNAME"));
+		fakeDataSource.setPass(pass);
+		fakeDataSource.setUrl(url);
+		return fakeDataSource;
+	}
+	
+	@Bean
+	public FakeJmsBroker fakeJmsBroker() {
+		FakeJmsBroker jmsBroker = new FakeJmsBroker();
+		jmsBroker.setUsername(jmsUsername);
+		jmsBroker.setPass(jmsPass);
+		jmsBroker.setUrl(jmsUrl);
+		return jmsBroker;
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		return propertySourcesPlaceholderConfigurer;
+	}
+
+}
